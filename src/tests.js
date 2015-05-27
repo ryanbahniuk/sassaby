41ad07897c6dac02c08436947c8e299a3b68824e
Ryan Bahniuk <ryan@wealthfronts-MacBook-Pro.local>
2015-05-27 13:18:02 -0700

    consistent calls

--- src/tests.js
+++ src/tests.js
@@ -7,24 +7,26 @@ describe('sample.scss', function() {
   
   describe('#appearance', function() {
     it('should return 3 declarations', function() {
-      sassafras.setCall(".test { @include appearance('button') }");
+      sassafras.setCall(".test { @include appearance(button) }");
       sassafras.assertDeclarationsNumber(3);
     });
 
     it('should have a webkit prefixed declaration', function() {
-      sassafras.setCall(".test { @include appearance('button') }");
+      sassafras.setCall(".test { @include appearance(button) }");
       sassafras.assertDeclaration("-webkit-appearance", "button");
-    });
-
-    it('should define the correct class', function() {
-      sassafras.setCall("@include make-column(md, 6);");
-      sassafras.assertSelectorCreation(".col-md-6");
     });
 
     it('should have the correct entire output', function() {
       sassafras.setCall(".test { @include appearance(button) }");
       var result = ".test { -webkit-appearance: button; -moz-appearance: button; appearance: button; }";
       sassafras.assertEntireOutput(result);
+    });
+  });
+
+  describe('#make-column', function() {
+    it('should define the correct class', function() {
+      sassafras.setCall("@include make-column(md, 6);");
+      sassafras.assertSelectorCreation(".col-md-6");
     });
   });
 });
