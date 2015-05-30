0bbc8a4d021416913251e595db38ef8d2fedba51
Ryan Bahniuk <ryan@wealthfronts-MacBook-Pro.local>
2015-05-30 15:19:55 -0700

    nesting constructors under assert

--- src/sassafras.js
+++ src/sassafras.js
@@ -13,16 +13,18 @@ var Sassafras = {
     this.file = fs.readFileSync(filename).toString();
   },
 
-  includedMixin: function(call) {
-    return new IncludedMixin(Sassafras.file, call);
-  },
+  assert: {
+    includedMixin: function(call) {
+      return new IncludedMixin(Sassafras.file, call);
+    },
 
-  standaloneMixin: function(call) {
-    return new StandaloneMixin(Sassafras.file, call);
-  },
+    standaloneMixin: function(call) {
+      return new StandaloneMixin(Sassafras.file, call);
+    },
 
-  func: function(call) {
-    return new Func(Sassafras.file, call);
+    func: function(call) {
+      return new Func(Sassafras.file, call);
+    }
   }
 };
 
--- tests.js
+++ tests.js
@@ -3,12 +3,13 @@
 'use strict';
 
 var sassafras = require('./src/sassafras');
+var assert = sassafras.assert;
 
 describe('sample.scss', function() {
   sassafras.setFile('sample.scss');
 
   describe('#appearance', function() {
-    var mixin = sassafras.includedMixin("appearance(button)");
+    var mixin = assert.includedMixin("appearance(button)");
 
     it('should return 3 declarations', function() {
       mixin.hasNDeclarations(3);
@@ -24,7 +25,7 @@ describe('sample.scss', function() {
   });
 
   describe('#make-column', function() {
-    var mixin = sassafras.standaloneMixin("make-column(md, 6)");
+    var mixin = assert.standaloneMixin("make-column(md, 6)");
 
     it('should define the correct class', function() {
       mixin.createsSelector(".col-md-6");
@@ -45,36 +46,36 @@ describe('sample.scss', function() {
 
   describe('#remy', function() {
     it('convert to px units to rem units', function() {
-      sassafras.func("remy(32px, 16px)").equals("2rem");
+      assert.func("remy(32px, 16px)").equals("2rem");
     });
   });
 
   describe('#boolean-switch', function() {
     it('should return true if passed true', function() {
-      sassafras.func("boolean-switch(true)").isTrue();
+      assert.func("boolean-switch(true)").isTrue();
     });
 
     it('should return false if passed false', function() {
-      sassafras.func("boolean-switch(false)").isFalse();
+      assert.func("boolean-switch(false)").isFalse();
     });
   });
 
   describe('#return-self', function() {
     it('testing truthy', function() {
-      sassafras.func("return-self(true)").isTruthy();
-      sassafras.func("return-self(1)").isTruthy();
-      sassafras.func("return-self('a')").isTruthy();
-      sassafras.func("return-self('')").isTruthy();
+      assert.func("return-self(true)").isTruthy();
+      assert.func("return-self(1)").isTruthy();
+      assert.func("return-self('a')").isTruthy();
+      assert.func("return-self('')").isTruthy();
     });
 
     it('testing falsey', function() {
-      sassafras.func("return-self(false)").isFalsey();
-      sassafras.func("return-self(null)").isFalsey();
+      assert.func("return-self(false)").isFalsey();
+      assert.func("return-self(null)").isFalsey();
     });
   });
 
   describe('#make-general-alignments', function() {
-    var mixin = sassafras.standaloneMixin("make-general-alignments(md)");
+    var mixin = assert.standaloneMixin("make-general-alignments(md)");
 
     it('should call the correct mixins', function() {
       mixin.calls("make-align-left(md)");
@@ -84,7 +85,7 @@ describe('sample.scss', function() {
   });
 
   describe('#animation', function() {
-    var mixin = sassafras.includedMixin("animation(test, 500)");
+    var mixin = assert.includedMixin("animation(test, 500)");
 
     it('should have the correct output', function() {
       mixin.declares("animation-name", "test");
