8c63579d221fffc358451ee846a601a42af3a824
Ryan Bahniuk <ryan@wealthfronts-MacBook-Pro.local>
2015-06-07 10:05:57 -0700

    adding mixin tests

---
+++ test/mixinTests.js
@@ -0,0 +1,53 @@
+'use strict';
+
+var assert = require('assert');
+var sinon = require('sinon');
+var proxyquire =  require('proxyquire');
+
+function MockMixinResult(type, file, call, args) {
+  this.type = type;
+  this.file = file;
+  this.call = call;
+  this.args = args;
+}
+
+var Mixin;
+var mixin;
+
+var type = 'test';
+var variables = '$test:5;';
+var dependencies = "@import 'file';";
+var file = '@mixin test($input) { color: $input }';
+var call = '@include test(red)';
+
+describe('Mixin', function() {
+  beforeEach(function() {
+    Mixin = proxyquire('../src/types/mixin', {
+      '../mixinResult': MockMixinResult
+    });
+    mixin = new Mixin(type, variables, dependencies, file, call);
+  });
+
+  describe('new', function() {
+    it('should set type, file, and call from the arguments', function() {
+      assert.equal(mixin.type, type);
+      assert.equal(mixin.file, variables + dependencies + file);
+      assert.equal(mixin.call, call);
+    });
+  });
+
+  describe('calledWith', function() {
+    it('should return a new mixinResult', function() {
+      var result = mixin.calledWith(1, 2, 'hello', true);
+      assert(result instanceof MockMixinResult)
+    });
+
+    it('should have the correct properties', function() {
+      var result = mixin.calledWith(1, 2, 'hello', true);
+      assert.equal(result.type, type);
+      assert.equal(result.file, variables + dependencies + file);
+      assert.equal(result.call, call);
+      assert.deepEqual(result.args, [1, 2, 'hello', true]);
+    });
+  });
+});
