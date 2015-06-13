5440df4b568dab156043d7c101f6dca1ede07ffd
Ryan Bahniuk <ryan@wealthfronts-MacBook-Pro.local>
2015-06-13 00:19:08 -0700

    removing deprecated method from integration tests

--- test/integrationTests.js
+++ test/integrationTests.js
@@ -14,24 +14,24 @@ describe('sample.scss', function() {
     });
 
     it('should return 3 declarations', function() {
-      mixin.calledWith('button').hasNumDeclarations(3);
+      mixin.calledWithArgs('button').hasNumDeclarations(3);
     });
 
     it('should have a webkit prefixed declaration', function() {
-      mixin.calledWith('button').declares('-webkit-appearance', 'button');
+      mixin.calledWithArgs('button').declares('-webkit-appearance', 'button');
     });
 
     it('should not make an incorrect declaration', function() {
-      mixin.calledWith('button').doesNotDeclare('-webkit-appearance', 'none');
-      mixin.calledWith('button').doesNotDeclare('color', 'red');
+      mixin.calledWithArgs('button').doesNotDeclare('-webkit-appearance', 'none');
+      mixin.calledWithArgs('button').doesNotDeclare('color', 'red');
     });
 
     it('should have the correct entire output', function() {
-      mixin.calledWith('button').equals('-webkit-appearance: button; -moz-appearance: button; appearance: button;');
+      mixin.calledWithArgs('button').equals('-webkit-appearance: button; -moz-appearance: button; appearance: button;');
     });
 
     it('should not equal the incorrect output', function() {
-      mixin.calledWith('button').doesNotEqual('-moz-appearance: button; appearance: button;');
+      mixin.calledWithArgs('button').doesNotEqual('-moz-appearance: button; appearance: button;');
     });
   });
 
@@ -41,39 +41,39 @@ describe('sample.scss', function() {
     });
 
     it('should define the correct class', function() {
-      mixin.calledWith('md', 6).createsSelector('.col-md-6');
+      mixin.calledWithArgs('md', 6).createsSelector('.col-md-6');
     });
 
     it('should not define the incorrect class', function() {
-      mixin.calledWith('md', 6).doesNotCreateSelector('.col-lg-6');
+      mixin.calledWithArgs('md', 6).doesNotCreateSelector('.col-lg-6');
     });
 
     it('should return 2 declarations', function() {
-      mixin.calledWith('md', 6).hasNumDeclarations(2);
+      mixin.calledWithArgs('md', 6).hasNumDeclarations(2);
     });
 
     it('should declare the correct width', function() {
-      mixin.calledWith('md', 6).declares('max-width', '50%');
+      mixin.calledWithArgs('md', 6).declares('max-width', '50%');
     });
 
     it('should not declare the incorrect width', function() {
-      mixin.calledWith('md', 6).doesNotDeclare('max-width', '60%');
-      mixin.calledWith('md', 6).doesNotDeclare('appearance', 'button');
+      mixin.calledWithArgs('md', 6).doesNotDeclare('max-width', '60%');
+      mixin.calledWithArgs('md', 6).doesNotDeclare('appearance', 'button');
     });
 
     it('should have the correct entire output', function() {
-      mixin.calledWith('md', 6).equals('.col-md-6 { flex-basis: 50%; max-width: 50%; }');
+      mixin.calledWithArgs('md', 6).equals('.col-md-6 { flex-basis: 50%; max-width: 50%; }');
     });
 
     it('should not have incorrect output', function() {
-      mixin.calledWith('md', 6).doesNotEqual('.col-md-8 { flex-basis: 50%; max-width: 50%; }');
+      mixin.calledWithArgs('md', 6).doesNotEqual('.col-md-8 { flex-basis: 50%; max-width: 50%; }');
     });
   });
 
   describe('make-sized-button', function() {
     beforeEach(function() {
       mixin = sassaby.standaloneMixin('make-sized-button');
-      compiled = mixin.calledWith('red', '200px');
+      compiled = mixin.calledWithArgs('red', '200px');
     });
 
     it('should create the correct media query', function() {
@@ -95,34 +95,34 @@ describe('sample.scss', function() {
 
   describe('remy', function() {
     it('convert to px units to rem units', function() {
-      sassaby.func('remy').calledWith('32px', '16px').equals('2rem');
+      sassaby.func('remy').calledWithArgs('32px', '16px').equals('2rem');
     });
 
     it('has the correct output unit', function() {
-      sassaby.func('remy').calledWith('32px', '16px').doesNotEqual('2em');
+      sassaby.func('remy').calledWithArgs('32px', '16px').doesNotEqual('2em');
     });
   });
 
   describe('boolean-switch', function() {
     it('should return true if passed true', function() {
-      sassaby.func('boolean-switch').calledWith(true).isTrue();
+      sassaby.func('boolean-switch').calledWithArgs(true).isTrue();
     });
 
     it('should return false if passed false', function() {
-      sassaby.func('boolean-switch').calledWith(false).isFalse();
+      sassaby.func('boolean-switch').calledWithArgs(false).isFalse();
     });
   });
 
   describe('return-self', function() {
     it('testing truthy', function() {
-      sassaby.func('return-self').calledWith(true).isTruthy();
-      sassaby.func('return-self').calledWith(1).isTruthy();
-      sassaby.func('return-self').calledWith('a').isTruthy();
+      sassaby.func('return-self').calledWithArgs(true).isTruthy();
+      sassaby.func('return-self').calledWithArgs(1).isTruthy();
+      sassaby.func('return-self').calledWithArgs('a').isTruthy();
     });
 
     it('testing falsy', function() {
-      sassaby.func('return-self').calledWith(false).isFalsy();
-      sassaby.func('return-self').calledWith(null).isFalsy();
+      sassaby.func('return-self').calledWithArgs(false).isFalsy();
+      sassaby.func('return-self').calledWithArgs(null).isFalsy();
     });
   });
 
@@ -132,13 +132,13 @@ describe('sample.scss', function() {
     });
 
     it('should call the correct mixins', function() {
-      mixin.calledWith('md').calls('make-align-left(md)');
-      mixin.calledWith('md').calls('make-align-center(md)');
-      mixin.calledWith('md').calls('make-align-right(md)');
+      mixin.calledWithArgs('md').calls('make-align-left(md)');
+      mixin.calledWithArgs('md').calls('make-align-center(md)');
+      mixin.calledWithArgs('md').calls('make-align-right(md)');
     });
 
     it('should not call the incorrect mixin', function() {
-      mixin.calledWith('md').doesNotCall('make-align-left(lg)');
+      mixin.calledWithArgs('md').doesNotCall('make-align-left(lg)');
     });
   });
 
@@ -148,19 +148,19 @@ describe('sample.scss', function() {
     });
 
     it('should have the correct output', function() {
-      mixin.calledWith('test', 500).declares('animation-name', 'test');
-      mixin.calledWith('test', 500).declares('animation-duration', 500);
+      mixin.calledWithArgs('test', 500).declares('animation-name', 'test');
+      mixin.calledWithArgs('test', 500).declares('animation-duration', 500);
     });
 
     it('should call the correct mixins', function() {
-      mixin.calledWith('test', 500).calls('prefixer(webkit, animation-name, test)');
-      mixin.calledWith('test', 500).calls('prefixer(moz, animation-name, test)');
-      mixin.calledWith('test', 500).calls('prefixer(webkit, animation-duration, 500)');
-      mixin.calledWith('test', 500).calls('prefixer(moz, animation-duration, 500)');
+      mixin.calledWithArgs('test', 500).calls('prefixer(webkit, animation-name, test)');
+      mixin.calledWithArgs('test', 500).calls('prefixer(moz, animation-name, test)');
+      mixin.calledWithArgs('test', 500).calls('prefixer(webkit, animation-duration, 500)');
+      mixin.calledWithArgs('test', 500).calls('prefixer(moz, animation-duration, 500)');
     });
 
     it('should not call the incorrect mixins', function() {
-      mixin.calledWith('test', 500).doesNotCall('prefixer(o, animation-name, test)');
+      mixin.calledWithArgs('test', 500).doesNotCall('prefixer(o, animation-name, test)');
     });
   });
 });
